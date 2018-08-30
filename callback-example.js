e91af82164246a515de11f894aec12a6d2ea6177
ankitaahuja111 <ankitaahuja111@gmail.com>
2018-08-30 16:28:41 +0000

    used forEach

--- callback-example.js
+++ callback-example.js
@@ -1,14 +1,31 @@
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
+  arr.forEach(function(element, i) {
+    if (element === "Waldo") {
       found(arr, i);   // execute callback
     }
-  }
+  })
 }
 
 function actionWhenFound(arr, i) {
-  // console.log("Found him!");
    console.log(`Found ${arr[i]} at index ${i}!`)
+ 
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+
+// function findWaldo(arr, found) {
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === "Waldo") {
+//       found(arr, i);   // execute callback
+//     }
+//   }
+// }
+
+// function actionWhenFound(arr, i) {
+//   // console.log("Found him!");
+//    console.log(`Found ${arr[i]} at index ${i}!`)
+// }
+
+// findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
